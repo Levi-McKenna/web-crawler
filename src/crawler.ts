258a06e074f3cebb1e5eeeb66bce5d97d033e9f8
Scufftistic <scufftistic@gmail.com>
2023-06-20 14:27:50 -0400

    initial branch commit

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,9 +1,11 @@
 export function normalizeURL(urlStr: string): string {
     const url = new URL(urlStr);
     const normalizedURL = `${url.hostname}${url.pathname}`;
-    if (normalizedURL.length > 0 && normalizedURL[normalizedURL.length-1] === '/'){ 
+    // removes all trailing forward slashes
+    if (normalizedURL.length > 0 && normalizedURL[normalizedURL.length-1] === '/'){
         return normalizedURL.slice(0, -1);
     } 
+    // if there are no tailing slashes return the string literal
     return normalizedURL;
 }
 
